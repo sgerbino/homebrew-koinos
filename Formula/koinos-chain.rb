f8ac412ba76444f3cee36162274f28eabcd80943
Steve Gerbino <steve@gerbino.co>
2022-11-20 03:03:35 -0500

    Add koinos-chain formula

---
+++ Formula/koinos-chain.rb
@@ -0,0 +1,20 @@
+class KoinosChain < Formula
+  desc "The chain microservice is responsible for the application of blocks, running of smart contracts, and applying transactions."
+  homepage "https://koinos.io"
+  url "https://github.com/koinos/koinos-chain/archive/refs/tags/v1.0.1.tar.gz"
+  version "1.0.1"
+  sha256 "95cdb17919997f4bcb16748a25e9e8106a0ab7184f44ac4a072ec9b833767ddf"
+  license "MIT"
+  head "https://github.com/koinos/koinos-chain.git"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
